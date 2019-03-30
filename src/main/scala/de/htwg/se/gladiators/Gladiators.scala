2cf0ee3b88cf9443d95b367893a037d73ed823c3
Sascha Villing <sascha.villing@web.de>
2019-03-30 09:49:30 +0100

    Erster Test Commit

--- src/main/scala/de/htwg/se/gladiators/Gladiators.scala
+++ src/main/scala/de/htwg/se/gladiators/Gladiators.scala
@@ -1,7 +1,8 @@
 package de.htwg.gladiators
 
 object Gladiators {
+
     def main(args: Array[String]): Unit = {
-        print("Gladiator");
+        print("Gladiator Gladiator Sascha ist ein krasser Gladiator");
     }
 }
