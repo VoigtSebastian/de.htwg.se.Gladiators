00f853bb64d637f43e08efb76c128ebdc49ddfe6
Sebastian Voigt <sebastian-voigt-kn@hotmail.de>
2019-04-11 13:53:07 +0200

    Adds a basic Cell test

--- src/main/scala/de/htwg/se/gladiators/Gladiators.scala
+++ src/main/scala/de/htwg/se/gladiators/Gladiators.scala
@@ -1,12 +1,25 @@
 package de.htwg.gladiators
 
 import de.htwg.se.gladiators.model.CellType
+import de.htwg.se.gladiators.model.PlayingField
 import de.htwg.se.gladiators.model.Cell
 
 object Gladiators {
 
     def main(args: Array[String]): Unit = {
-        print("Gladiators");
-        val cell = Cell(CellType.BASE.id);
+        println("Gladiators");
+        val cells = Array.ofDim[Cell](3, 3)
+        cells(0)(0) = Cell(CellType.BASE.id)
+        cells(0)(1) = Cell(CellType.SAND.id)
+        cells(0)(2) = Cell(CellType.WATER.id)
+        cells(1)(0) = Cell(CellType.SAND.id)
+        cells(1)(1) = Cell(CellType.SAND.id)
+        cells(1)(2) = Cell(CellType.WATER.id)
+        cells(2)(0) = Cell(CellType.SAND.id)
+        cells(2)(1) = Cell(CellType.SAND.id)
+        cells(2)(2) = Cell(CellType.BASE.id)
+
+        val field = PlayingField(cells);
+        println(field.toString())
     }
 }
