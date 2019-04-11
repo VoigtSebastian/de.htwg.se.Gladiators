44c04565c86ff4874e6e5940f5c6c3e2cacd09b8
Sebastian Voigt <sebastian-voigt-kn@hotmail.de>
2019-04-11 11:42:18 +0200

    Adds Cell, cellTypes and playingField

--- src/main/scala/de/htwg/se/gladiators/Gladiators.scala
+++ src/main/scala/de/htwg/se/gladiators/Gladiators.scala
@@ -1,8 +1,12 @@
 package de.htwg.gladiators
 
+import de.htwg.se.gladiators.model.CellType
+import de.htwg.se.gladiators.model.Cell
+
 object Gladiators {
 
     def main(args: Array[String]): Unit = {
         print("Gladiators");
+        val cell = Cell(CellType.BASE.id);
     }
 }
