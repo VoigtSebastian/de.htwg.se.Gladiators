995c93080c883718872bb2c04ab8d75fe755e1a1
Sebastian Voigt <sebastian-voigt-kn@hotmail.de>
2019-04-11 11:42:18 +0200

    Adds Cell, cellTypes and playingField

---
+++ src/main/scala/de/htwg/se/gladiators/model/Cell.scala
@@ -0,0 +1,9 @@
+package de.htwg.se.gladiators.model
+
+case class Cell(cellType: Int) {
+
+    def getCellType(): Unit = {
+        return cellType
+    }
+
+}
---
+++ src/main/scala/de/htwg/se/gladiators/model/CellTypes.scala
@@ -0,0 +1,6 @@
+package de.htwg.se.gladiators.model
+
+object CellType extends Enumeration {
+    type CellType = Value;
+    val BASE, SAND, MARSH, WATER = Value;
+}
