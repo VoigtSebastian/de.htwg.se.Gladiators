fd7a45254b42fa4dab1b7e02df77bd0e013ae86d
Sebastian Voigt <sebastian-voigt-kn@hotmail.de>
2019-04-12 09:44:33 +0200

    Adds some tests

--- src/test/scala/de/htwg/se/gladiators/model/model/GladiatorSpec.scala
+++ src/test/scala/de/htwg/se/gladiators/model/model/GladiatorSpec.scala
@@ -1,37 +1,37 @@
 package de.htwg.se.gladiators.model.model
 
 import de.htwg.se.gladiators.model.{Cell, Gladiator}
-import de.htwg.se.gladiators.model.GaldiatorType._
+import de.htwg.se.gladiators.model.GladiatorType._
 import org.scalatest.{Matchers, WordSpec}
 
 class GladiatorSpec extends WordSpec with Matchers {
   "A Gladiator is a fighter in the game. A Gladiator" when {
 
     "new" should {
-      var gladiator = Gladiator(new Cell(0), 10.0, 50.0, 100.0, SWORD)
+      var gladiator = Gladiator(Cell(0), 10.0, 50.0, 100.0, SWORD)
 
       "have a cell" in {
-        gladiator.cell should be(Cell(0));
+        gladiator.cell should be(Cell(0))
       }
       "have movementPoints" in {
-        gladiator.movementPoints should be (10.0);
+        gladiator.movementPoints should be (10.0)
       }
       "have ap" in {
-        gladiator.ap should be (50.0);
+        gladiator.ap should be (50.0)
       }
       "have hp" in {
-        gladiator.hp should be (100.0);
+        gladiator.hp should be (100.0)
       }
       "have a type " in {
-        gladiator.gladiatortype should be (SWORD);
+        gladiator.gladiatortype should be (SWORD)
       }
     }
     "after levelUp" should {
-      var gladiator2 = Gladiator(new Cell(0), 10.0, 50.0, 100.0, SWORD)
+      var gladiator2 = Gladiator(Cell(0), 10.0, 50.0, 100.0, SWORD)
 
-      gladiator2.levelUp(50);
+      gladiator2.levelUp(50)
       "have more ap" in {
-        gladiator2.ap should be (100.0);
+        gladiator2.ap should be (100.0)
       }
     }
 
