ccf2d9795c7eb0075a4e9de53e62b1281297beeb
Sascha Villing <sascha.villing@web.de>
2019-04-12 08:21:26 +0200

    changes PlayerSpec

---
+++ src/test/scala/de/htwg/se/gladiators/model/model/PlayerSpec.scala
@@ -0,0 +1,17 @@
+package de.htwg.se.gladiators.model.model
+
+import de.htwg.se.gladiators.model.Player
+
+import org.scalatest.{Matchers, WordSpec}
+
+class PlayerSpec extends WordSpec with Matchers{
+  "A Player" when { "new" should {
+    val player = Player("Your Name")
+    "have a name"  in {
+      player.name should be("Your Name")
+    }
+    "have a nice String representation" in {
+      player.toString should be("Your Name")
+    }
+  }}
+}
