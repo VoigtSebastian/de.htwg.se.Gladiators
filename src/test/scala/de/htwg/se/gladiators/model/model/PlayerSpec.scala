e5591e501473de7928517e43bb8e730e6a67e006
Sascha Villing <sascha.villing@web.de>
2019-04-12 08:47:40 +0200

    changes PlayerSpec, GladiatorSpec adds PlayingFieldSpec

--- src/test/scala/de/htwg/se/gladiators/model/model/PlayerSpec.scala
+++ src/test/scala/de/htwg/se/gladiators/model/model/PlayerSpec.scala
@@ -6,8 +6,8 @@ import org.scalatest.{Matchers, WordSpec}
 
 class PlayerSpec extends WordSpec with Matchers{
   "A Player" when { "new" should {
-    val player = Player("Your Name")
-    "have a name"  in {
+      val player = Player("Your Name")
+      "have a name"  in {
       player.name should be("Your Name")
     }
     "have a nice String representation" in {
