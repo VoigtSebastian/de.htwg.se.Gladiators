83a890192e222d15200af625ba8a1e90d06316b2
Sebastian Voigt <sebastian-voigt-kn@hotmail.de>
2019-04-26 12:47:46 +0200

    adds formatLine to print line with gladiators

--- src/main/scala/de/htwg/se/gladiators/Gladiators.scala
+++ src/main/scala/de/htwg/se/gladiators/Gladiators.scala
@@ -1,19 +1,12 @@
 package de.htwg.se.gladiators
 
-
-import de.htwg.se.gladiators.model.{Cell, CellType, Gladiator, PlayingField}
+import de.htwg.se.gladiators.model.{Cell, CellType, Gladiator, GladiatorType, PlayingField}
 
 object Gladiators {
 
     def main(args: Array[String]): Unit = {
         println("Gladiators")
         val cells = Array.ofDim[Cell](3, 3)
-
-        for (i <- cells.indices) {
-            for (j <- cells(i).indices) {
-                cells(i)(j) = Cell(CellType.SAND.id)
-            }
-        }
         cells(0)(0) = Cell(CellType.BASE.id)
         cells(0)(1) = Cell(CellType.SAND.id)
         cells(0)(2) = Cell(CellType.WATER.id)
@@ -23,10 +16,14 @@ object Gladiators {
         cells(2)(0) = Cell(CellType.SAND.id)
         cells(2)(1) = Cell(CellType.SAND.id)
         cells(2)(2) = Cell(CellType.BASE.id)
-
         val field = PlayingField(cells)
+
+        var glad: List[Gladiator] = List(Gladiator(0,0, 1.0,1.0,1.0, GladiatorType.MAGIC),
+            Gladiator(1,0, 1.0,1.0,1.0, GladiatorType.SWORD))
+
+        println("Line zero with gladiators: " + field.formatLine(0,glad) + "\n\n")
+
         println(field.toString())
 
-        println("CellType.SAND prints: " + CellType.SAND)
     }
 }
