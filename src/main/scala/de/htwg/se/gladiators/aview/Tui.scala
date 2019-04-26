485825371e6fb14b6e129fc6eb76ea8765dc516d
Sascha Villing <43783342+v1lling@users.noreply.github.com>
2019-04-26 12:49:58 +0200

    Update Tui.scala

--- src/main/scala/de/htwg/se/gladiators/aview/Tui.scala
+++ src/main/scala/de/htwg/se/gladiators/aview/Tui.scala
@@ -1,5 +1,27 @@
-package de.htwg.se.gladiators.aview
+package de.htwg.se.sudoku.aview
+
+import de.htwg.se.gladiators.model.{Cell,Gladiator,Player,PlayingField}
+
 
 class Tui {
 
+  def processInputLine(input: String, pf:PlayingField):PlayingField = {
+
+    input match {
+
+      case "q" => pf
+      case "n"=> pf.createRandom(7);
+      //case "g" =>
+      /*
+       case _ => {
+
+         input.toList.filter(c => c != ' ').map(c => c.toString.toInt) match {
+           case row :: column :: value :: Nil => grid.set(row, column, value)
+           case _ => grid
+       */
+      }
+
+  }
+
 }
+
