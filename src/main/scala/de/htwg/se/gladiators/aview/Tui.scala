18f187175e12347256a9ddd6926d064cd9df2974
VoigtSebastian <sebastian-voigt-kn@hotmail.de>
2019-05-02 18:57:30 +0200

    Add a working Controller Class in combination with an evaluator for the TUI.

--- src/main/scala/de/htwg/se/gladiators/aview/Tui.scala
+++ src/main/scala/de/htwg/se/gladiators/aview/Tui.scala
@@ -1,37 +1,17 @@
 package de.htwg.se.gladiators.aview
 
-import de.htwg.se.gladiators.model.{Cell, Gladiator, Player, PlayingField}
+import de.htwg.se.gladiators.controller.TuiController
 
 
-class Tui {
+class Tui (controller: TuiController){
 
-    //evalPrintLine formatters
-    val SAND_BACKGROUND = "\033[103m"
-    val PALM_BACKGROUND = "\033[43m"
-    val BASE_BACKGROUND = "\033[101m"
-    val UNIT_BACKGROUND = "\033[45m"
-    val TEXT_COLOR_BLACK = "\33[97m"
-    val RESET_ANSI_ESCAPE = "\033[0m"
-
-    def generateHelpMessage(): String = {
-        val ret: String = "Gladiators instructions:\n-description-\n" +
-            TEXT_COLOR_BLACK + ""  + SAND_BACKGROUND + " color of a sand tile" + RESET_ANSI_ESCAPE + "\n" +
-            TEXT_COLOR_BLACK + ""  + PALM_BACKGROUND + " color of a palm tile" + RESET_ANSI_ESCAPE + "\n" +
-            TEXT_COLOR_BLACK + ""  + BASE_BACKGROUND + " color of a base tile" + RESET_ANSI_ESCAPE + "\n" +
-            TEXT_COLOR_BLACK + ""  + UNIT_BACKGROUND +
-            " color of a unit tile (S = Sword unit, B = Bow unit, M = Magic unit" +
-            RESET_ANSI_ESCAPE + "\n\nYour current playingField!:"
-        ret
-    }
-
-    def processInputLine(input: String, pf: PlayingField): (PlayingField, String) = {
-
+    def processInputLine(input: String): String = {
         input match {
-            case "q" => (pf, "")
-            case "n" => (pf.createRandom(11), "New playingFiled created: ")
-            case "h" => (pf, generateHelpMessage())
-            case "t" => (pf, "implement toggle unit color?")
-            case _=> (pf, generateHelpMessage()) //TODO: Make units addressable
+            case "q" => ""
+            case "n" => controller.createRandom(); controller.printPlayingField(); "New playingFiled created: "
+            case "h" => controller.printHelpMessage(); ""
+            case "t" => "implement toggle unit color?"
+            case _=> controller.createCommand(input).toString()
             //case "g" =>
             /*
              case _ => {
@@ -42,6 +22,10 @@ class Tui {
              */
         }
 
+    }
+
+    def showOutput(output: String): Unit = {
+        println(output)
     }
 
 
---
+++ src/main/scala/de/htwg/se/gladiators/controller/TuiController.scala
@@ -0,0 +1,47 @@
+package de.htwg.se.gladiators.controller
+
+import de.htwg.se.gladiators.model._
+import de.htwg.se.gladiators.aview.Tui
+
+class TuiController {
+    val INPUT_BLUE = "\u001B[34m"
+    val WAITING_FOR_INPUT:String = INPUT_BLUE + "▶ " + TuiEvaluator.RESET_ANSI_ESCAPE
+    val DIMENSIONS = 7
+
+    var tui = new Tui(this)
+    var playingField: PlayingField = PlayingField(Array.ofDim[Cell](5, 5)).createRandom(DIMENSIONS)
+    var gladiators = List(Gladiator(0, 0, 1.0, 1.0, 1.0, GladiatorType.MAGIC),
+        Gladiator(0, 1, 1.0, 1.0, 1.0, GladiatorType.SWORD), Gladiator(0, 2, 1.0, 1.0, 1.0, GladiatorType.SWORD),
+        Gladiator(1, 3, 1.0, 1.0, 1.0, GladiatorType.SWORD), Gladiator(3, 3, 1.0, 1.0, 1.0, GladiatorType.SWORD))
+
+    def gameLoop (): Unit = {
+        var input: String = ""
+        var output: String = ""
+        do {
+            print(WAITING_FOR_INPUT)
+            input = scala.io.StdIn.readLine()
+
+            output = tui.processInputLine(input)
+
+            tui.showOutput(output)
+
+        } while (input != "q")
+    }
+
+    def printHelpMessage(): Unit = {
+        tui.showOutput(TuiEvaluator.generateHelpMessage())
+    }
+
+    def createRandom(): Unit = {
+        playingField = playingField.createRandom(DIMENSIONS)
+    }
+
+    def createCommand(command:String): Vector[String] = {
+        TuiEvaluator.evalCommand(command)
+    }
+
+    def printPlayingField(): Unit = {
+        for (i <- playingField.cells.indices)
+            tui.showOutput(TuiEvaluator.evalPrintLine(playingField.formatLine(i, gladiators)))
+    }
+}
