43ffe65130d27fe33ffc2ea8cdd89bbcb208b79f
Sascha Villing <sascha.villing@web.de>
2019-05-03 09:38:07 +0200

    changes Controller, Gladiators

--- src/main/scala/de/htwg/se/gladiators/controller/Controller.scala
+++ src/main/scala/de/htwg/se/gladiators/controller/Controller.scala
@@ -2,18 +2,11 @@ package de.htwg.se.gladiators.controller
 
 import de.htwg.se.gladiators.model._
 import de.htwg.se.gladiators.aview.Tui
+import de.htwg.se.gladiators.model.GladiatorType.GladiatorType
 import de.htwg.se.gladiators.util.Observable
 
-class Controller extends Observable {
+class Controller(var playingField : PlayingField) extends Observable {
     val DIMENSIONS = 7
-    var playingField: PlayingField = PlayingField(Array.ofDim[Cell](5, 5)).createRandom(DIMENSIONS)
-    var gladiators = List(Gladiator(0, 0, 1.0, 1.0, 1.0, GladiatorType.TANK),
-        Gladiator(0, 1, 1.0, 1.0, 1.0, GladiatorType.SWORD),
-        Gladiator(0, 2, 1.0, 1.0, 1.0, GladiatorType.SWORD),
-        Gladiator(1, 3, 1.0, 1.0, 1.0, GladiatorType.SWORD),
-        Gladiator(3, 3, 1.0, 1.0, 1.0, GladiatorType.SWORD))
-
-
 
     def printHelpMessage(): String = {
         TuiEvaluator.generateHelpMessage()
@@ -34,5 +27,9 @@ class Controller extends Observable {
             str = str + TuiEvaluator.evalPrintLine(playingField.formatLine(i)) + "\n"
         str*/
         playingField.toString()
+    }
+    def addGladiator(x: Int, y: Int, movementPoints: Double, ap: Double, hp: Double, gladiatorType: GladiatorType): Unit = {
+        var glad = new Gladiator(x, y, movementPoints, ap, hp, gladiatorType)
+        playingField = playingField.createGladiator(glad)
     }
 }
