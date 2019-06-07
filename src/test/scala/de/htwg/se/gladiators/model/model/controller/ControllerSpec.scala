553f762ec639fb1ac0adbfa96aec07df4e2c1130
Sebastian Voigt <sebastian-voigt-kn@hotmail.de>
2019-06-07 11:03:56 +0200

    Add tests and remove obsolete tests

--- src/test/scala/de/htwg/se/gladiators/model/model/controller/ControllerSpec.scala
+++ src/test/scala/de/htwg/se/gladiators/model/model/controller/ControllerSpec.scala
@@ -6,32 +6,47 @@ import de.htwg.se.gladiators.util.Observer
 import org.scalatest.{Matchers, WordSpec}
 
 class ControllerSpec extends WordSpec with Matchers {
-  "A Controller" when {
-    "observed by an Observer" should {
-      val controller = new Controller(PlayingField())
-      val observer = new Observer {
-        var updated: Boolean = false
+    "A controller" when {
+        val controller = new Controller(PlayingField())
+        val xLegal = 5
+        val yLegal = 5
 
-        def isUpdated: Boolean = updated
-
-        override def update: Unit = {
-          updated = true
-          print(controller.printPlayingField())
-          //updated
+        val xIllegal = 10
+        val yIllegal = 12
+        "when checking coordinates " + xLegal + " " + yLegal in {
+            controller.isCoordinateLegal(xLegal, yLegal) should be(true)
+            controller.isCoordinateLegal(xIllegal, yIllegal) should be(false)
         }
-      }
-     // controller.add(observer)
-      "notify its Observer after random creation" in {
-        controller.createRandom()
-        observer.updated should be(true)
-      }
-      "notify its Observer after adding a gladiator" in {
-        controller.addGladiator(0, 0,  GladiatorType.SWORD)
-        controller.addGladiator(1, 1, GladiatorType.BOW)
-        controller.addGladiator(2, 2, GladiatorType.TANK)
-        observer.updated should be(true)
-        controller.playingField.gladiatorPlayer1.head.movementPoints should be (3)
-      }
     }
-  }
+
+    //TODO: ControllerSpecUpdate
+    //"A Controller" when {
+    //    "observed by an Observer" should {
+    //        val controller = new Controller(PlayingField())
+    //
+    //        val observer = new Observer {
+    //            var updated: Boolean = false
+    //
+    //            def isUpdated: Boolean = updated
+    //
+    //            override def update: Unit = {
+    //                updated = true
+    //                print(controller.printPlayingField())
+    //                //updated
+    //            }
+    //        }
+    //        controller.add(observer)
+    //        "notify its Observer after random creation" in {
+    //            controller.createRandom() observer.updated should be(true)
+    //        }
+    //        "notify its Observer after adding a gladiator" in {
+    //            controller.addGladiator(0, 0, GladiatorType.SWORD)
+    //            controller.addGladiator(1, 1, GladiatorType.BOW)
+    //            controller.addGladiator(2, 2, GladiatorType.TANK)
+    //            observer.updated should be(true)
+    //            controller.playingField.gladiatorPlayer1.head.movementPoints should be(3)
+    //        }
+    //    }
+    //}
+
 }
