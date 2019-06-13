e6a3e5d05d3aad5aab9d3190a8d3a20b8e694518
Sebastian Voigt <sebastian-voigt-kn@hotmail.de>
2019-06-13 14:59:15 +0200

    Fix tests

--- src/test/scala/de/htwg/se/gladiators/model/model/aview/TuiSpec.scala
+++ src/test/scala/de/htwg/se/gladiators/model/model/aview/TuiSpec.scala
@@ -1,26 +1,27 @@
 package de.htwg.se.gladiators.model.model.aview
 
 import de.htwg.se.gladiators.aview.Tui
-import de.htwg.se.gladiators.controller.Controller
+import de.htwg.se.gladiators.controller.{Controller, GameStatus}
 import de.htwg.se.gladiators.model.{Cell, Gladiator, GladiatorType, PlayingField}
 import org.scalatest.{Matchers, WordSpec}
 
 class TuiSpec extends WordSpec with Matchers {
-  val controller = new Controller(PlayingField())
-  val tui = new Tui(controller)
-  "do nothing on input 'q'" in {
-    tui.processInputLine("q")
-  }
-  "create a random playingfield" in {
-    tui.processInputLine("n")
-    controller.playingField.getSize should be (7)
-  }
+    val controller = new Controller(PlayingField())
+    val tui = new Tui(controller)
+    "do nothing on input 'q'" in {
+        tui.processInputLine("q")
+    }
+    "create a random playingfield" in {
+        tui.processInputLine("n")
+        controller.playingField.getSize should be(7)
+    }
 
-  "add a gladiator to the playingfield" in {
-    tui.processInputLine("g,0,0")
-    controller.playingField.gladiatorPlayer1.head should be (Gladiator(0,0,3,50,100,GladiatorType.SWORD,controller.players(0)))
-  }
-  "show a help message" in {
-    tui.processInputLine("h")
-  }
+    "add a gladiator to the playingfield" in {
+        controller.gameStatus = GameStatus.P1
+        tui.processInputLine("g,0,0")
+        controller.playingField.gladiatorPlayer1.head should be(Gladiator(0, 0, 3, 50, 100, GladiatorType.SWORD, controller.players(0)))
+    }
+    "show a help message" in {
+        tui.processInputLine("h")
+    }
 }
