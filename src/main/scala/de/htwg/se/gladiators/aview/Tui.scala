7bb2b12c2cbf4003b349f7d23e070f4bab4fb1a5
VoigtSebastian <sebastian-voigt-kn@hotmail.de>
2019-06-23 13:47:10 +0200

    Add safe casting commands for the Tui, change return types of move and attack to a tuple of (Boolean, String) so that they can be used for Gui and Tui operations, correct formatting errors.

--- src/main/scala/de/htwg/se/gladiators/aview/Tui.scala
+++ src/main/scala/de/htwg/se/gladiators/aview/Tui.scala
@@ -7,7 +7,7 @@ import de.htwg.se.gladiators.util.Observer
 
 import scala.swing.Reactor
 
-class Tui (controller: Controller) extends Reactor {
+class Tui(controller: Controller) extends Reactor {
 
     //controller.add(this)
     listenTo(controller)
@@ -21,10 +21,10 @@ class Tui (controller: Controller) extends Reactor {
 
     val CORRECT_FORMAT_MESSAGE = "Please use the correct format, press h to get help.\n"
     val HELP_MESSAGE: String = "Gladiators instructions:\n-description-\n" +
-        TEXT_COLOR_BLACK + ""  + SAND_BACKGROUND + " color of a sand tile" + RESET_ANSI_ESCAPE + "\n" +
-        TEXT_COLOR_BLACK + ""  + PALM_BACKGROUND + " color of a palm tile" + RESET_ANSI_ESCAPE + "\n" +
-        TEXT_COLOR_BLACK + ""  + BASE_BACKGROUND + " color of a base tile" + RESET_ANSI_ESCAPE + "\n" +
-        TEXT_COLOR_BLACK + ""  + UNIT_BACKGROUND +
+        TEXT_COLOR_BLACK + "" + SAND_BACKGROUND + " color of a sand tile" + RESET_ANSI_ESCAPE + "\n" +
+        TEXT_COLOR_BLACK + "" + PALM_BACKGROUND + " color of a palm tile" + RESET_ANSI_ESCAPE + "\n" +
+        TEXT_COLOR_BLACK + "" + BASE_BACKGROUND + " color of a base tile" + RESET_ANSI_ESCAPE + "\n" +
+        TEXT_COLOR_BLACK + "" + UNIT_BACKGROUND +
         " color of a unit tile (S = Sword unit, B = Bow unit, T = Tank unit" +
         RESET_ANSI_ESCAPE + "\n"
 
@@ -42,10 +42,10 @@ class Tui (controller: Controller) extends Reactor {
             case "m" =>
                 moveCommandBuilder(splitinput) match {
                     case Some(moveCommand) =>
-                        controller.moveGladiator(moveCommand._1,
-                        moveCommand._2,
-                        moveCommand._3,
-                        moveCommand._4)
+                        println(controller.moveGladiator(moveCommand._1,
+                            moveCommand._2,
+                            moveCommand._3,
+                            moveCommand._4)._2)
                     case None => println(CORRECT_FORMAT_MESSAGE)
                 }
 
@@ -53,21 +53,32 @@ class Tui (controller: Controller) extends Reactor {
             case "r" => controller.redoGladiator()
 
             case "a" =>
-                if (splitinput.size == 5)
-                    println(controller.attack(splitinput(1).toInt, splitinput(2).toInt, splitinput(3).toInt, splitinput(4).toInt))
-                else
-                    println(CORRECT_FORMAT_MESSAGE)
-
+                attackCommandBuilder(splitinput) match {
+                    case Some(attackCommand) =>
+                        println(controller.attack(attackCommand._1,
+                            attackCommand._2,
+                            attackCommand._3,
+                            attackCommand._4)._2)
+                    case None => println(CORRECT_FORMAT_MESSAGE)
+                }
             case "i" =>
-                if (splitinput.size == 3)
-                    println(controller.gladiatorInfo(splitinput(1).toInt, splitinput(2).toInt))
-                else
-                    println(CORRECT_FORMAT_MESSAGE)
+                infoCommandBuilder(splitinput) match {
+                    case Some(infoCommand) =>
+                        println(controller.gladiatorInfo(infoCommand._1, infoCommand._2))
+                    case None => println(CORRECT_FORMAT_MESSAGE)
+                }
             case "s" => println(controller.getShop)
-            case "b" => println(controller.buyGladiator(splitinput(1).toInt, splitinput(2).toInt, splitinput(3).toInt))
+            case "b" =>
+                buyCommandBuilder(splitinput) match {
+                    case Some(buyCommand) =>
+                        println(controller.buyGladiator(buyCommand._1,
+                            buyCommand._2,
+                            buyCommand._3))
+                    case None => println(CORRECT_FORMAT_MESSAGE)
+                }
             case "e" => println(controller.endTurn())
 
-            case _=> println(splitinput.toString())
+            case _ => println(splitinput.toString())
         }
     }
 
@@ -76,13 +87,43 @@ class Tui (controller: Controller) extends Reactor {
         case event: GladChanged => printPf()
     }
 
+    def buyCommandBuilder(v: Vector[String]): Option[(Int, Int, Int)] = {
+        if (v.size != 4)
+            None
+        val ret = (toInt(v(1)), toInt(v(2)), toInt(v(3)))
+        ret match {
+            case (Some(i), Some(j), Some(k)) => Some(i, j, k)
+            case _ => None
+        }
+    }
+
+    def infoCommandBuilder(v: Vector[String]): Option[(Int, Int)] = {
+        if (v.size != 3)
+            None
+        val ret = (toInt(v(1)), toInt(v(2)))
+        ret match {
+            case (Some(i), Some(j)) => Some(i, j)
+            case _ => None
+        }
+    }
+
+    def attackCommandBuilder(v: Vector[String]): Option[(Int, Int, Int, Int)] = {
+        if (v.size != 5)
+            None
+        val ret = (toInt(v(1)), toInt(v(2)), toInt(v(3)), toInt(v(4)))
+        ret match {
+            case (Some(i), Some(j), Some(k), Some(l)) => Some(i, j, k, l)
+            case _ => None
+        }
+    }
+
     def moveCommandBuilder(v: Vector[String]): Option[(Int, Int, Int, Int)] = {
         if (v.size != 5)
             None
         val ret = (toInt(v(1)), toInt(v(2)), toInt(v(3)), toInt(v(4)))
         ret match {
             case (Some(i), Some(j), Some(k), Some(l)) => Some(i, j, k, l)
-            case _=> None
+            case _ => None
         }
     }
 
