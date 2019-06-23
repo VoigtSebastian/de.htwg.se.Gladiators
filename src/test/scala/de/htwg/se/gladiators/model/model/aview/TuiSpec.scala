01cdb4dfae135368dd2e8cb8cfc2865a15e5a155
VoigtSebastian <sebastian-voigt-kn@hotmail.de>
2019-06-23 14:41:42 +0200

    Fix TuiSpec tests

--- src/test/scala/de/htwg/se/gladiators/model/model/aview/TuiSpec.scala
+++ src/test/scala/de/htwg/se/gladiators/model/model/aview/TuiSpec.scala
@@ -8,20 +8,21 @@ import org.scalatest.{Matchers, WordSpec}
 class TuiSpec extends WordSpec with Matchers {
     val controller = new Controller(PlayingField())
     val tui = new Tui(controller)
-    controller.createRandom(7,0)
+    controller.createRandom(9,0)
     "do nothing on input 'q'" in {
         tui.processInputLine("q")
     }
-    "create a random playingfield" in {
+    "create a random playingField" in {
         tui.processInputLine("n")
         controller.playingField.getSize should be(9)
     }
 
-    "add a gladiator to the playingfield" in {
+    "add a gladiator to the playingField" in {
+        controller.createRandom(9,0)
         controller.gameStatus = GameStatus.P1
-        tui.processInputLine("b 1 7 4")
-        controller.playingField.gladiatorPlayer1.head.line should be(7)
-        controller.playingField.gladiatorPlayer1.head.row should be(4)
+        tui.processInputLine("b 1 8 5")
+        controller.playingField.gladiatorPlayer1.head.line should be(8)
+        controller.playingField.gladiatorPlayer1.head.row should be(5)
     }
 
     "show a help message" in {
