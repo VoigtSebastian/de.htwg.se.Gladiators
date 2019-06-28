4c060bac7d1361ee1761ec4b639b4a8379f9c3d3
Sebastian Voigt <sebastian-voigt-kn@hotmail.de>
2019-06-28 09:32:38 +0200

    Fix the ControllerInterface.

--- src/main/scala/de/htwg/se/gladiators/controller/controllerComponent/ControllerInterface.scala
+++ src/main/scala/de/htwg/se/gladiators/controller/controllerComponent/ControllerInterface.scala
@@ -26,10 +26,29 @@ trait ControllerInterface extends Publisher {
 
   def cell(line: Int, row: Int): Cell
 
-  def resetGame(): Controller
+  /**
+    * Resets the game and return a reset ControllerInterface implementation.
+    * @return a reset ControllerInterface implementation
+    */
+  def resetGame(): ControllerInterface
+
+  /**
+    * Ends the turn for the current player.
+    * @return String containing a message that can be shown in a Tui.
+    */
   def endTurn(): String
+
+  /**
+    * Creates a random PlayingField, giving all cells a new CellType.
+    * @param size: The size of the new playing field.
+    * @param palmRate: The rate at which palm should appear between 0 and 100.
+    */
   def createRandom(size: Int, palmRate: Int = 17): Unit
 
+  /**
+    * Returns the current shop.
+    * @return current shop.
+    */
   def getShop: String
 
   def printPlayingField(): String
@@ -58,7 +77,7 @@ trait ControllerInterface extends Publisher {
   def getGladiatorOption(line: Int, row: Int): Option[Gladiator]
   def cellSelected(line: Int, row: Int): Unit
 
-  def changeCommand(commandStatus: CommandStatus): Controller
+  def changeCommand(commandStatus: CommandStatus): ControllerInterface
 
   def categorizeMove(lineStart: Int, rowStart: Int, lineDest: Int, rowDest: Int): MoveType
   def isGladiatorInList(list: List[Gladiator], line: Int, row: Int): Boolean
