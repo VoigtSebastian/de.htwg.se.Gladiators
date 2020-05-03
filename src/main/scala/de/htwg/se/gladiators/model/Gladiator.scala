9aadc20009cb004739f350981b3634afd3290e76
Sascha Villing <sascha.villing@web.de>
2020-05-03 10:36:27 +0200

    Remove Glaidator LevelUp Method

--- src/main/scala/de/htwg/se/gladiators/model/Gladiator.scala
+++ src/main/scala/de/htwg/se/gladiators/model/Gladiator.scala
@@ -5,10 +5,6 @@ import de.htwg.se.gladiators.model.GladiatorType._
 
 case class Gladiator(var line: Int, var row: Int, movementPoints: Double, var ap: Double, var hp: Double, gladiatorType: GladiatorType, var player: Player, var moved: Boolean = true) {
 
-    def levelUp(value: Int): Unit = {
-        this.ap += value
-    }
-
     def move(line: Int, row: Int): Unit = {
         this.line = line
         this.row = row
