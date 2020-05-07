fbf959aa66560340941022244f3547cfaed1da3f
Sebastian Voigt <sebastian.m.voigt@gmail.com>
2020-05-07 17:20:53 +0200

    Remove deprecated and copied test

--- src/test/scala/de/htwg/se/gladiators/model/model/controller/MoveGladiatorCommandSpec.scala
+++ src/test/scala/de/htwg/se/gladiators/model/model/controller/MoveGladiatorCommandSpec.scala
@@ -6,27 +6,5 @@ import de.htwg.se.gladiators.model.playingFieldComponent.playingFieldBaseImpl.Pl
 import org.scalatest.{Matchers, WordSpec}
 
 class MoveGladiatorCommandSpec extends WordSpec with Matchers {
-
-  var controller = new Controller()
-  controller.createRandom(15, 0)
-  controller.addGladiator(13, 7)
-  val command = new MoveGladiatorCommand(13, 7, 13, 8, controller)
-  "A MoveGladiatorcommand" when {
-    "create a move command" in {
-
-      controller.gameStatus = GameStatus.P1
-      command.doStep
-      controller.playingField.gladiatorPlayer1.head.row should be (8)
-    }
-    "undo that step " in {
-      controller.gameStatus = GameStatus.P1
-      command.undoStep
-      controller.playingField.gladiatorPlayer1.head.row should be (7)
-    }
-    "redo that step again " in {
-      controller.gameStatus = GameStatus.P1
-      command.redoStep
-      controller.playingField.gladiatorPlayer1.head.row should be (8)
-    }
-  }
+  // Todo
 }
