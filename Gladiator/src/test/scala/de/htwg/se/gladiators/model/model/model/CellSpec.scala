0aa951bf9bf2ea65678a74720225a8ba13a4f7e9
Sascha Villing <sascha.villing@web.de>
2020-05-20 11:56:58 +0200

    Add Microservices for GladiatorType and Gladiator

---
+++ Gladiator/src/test/scala/de/htwg/se/gladiators/model/model/model/CellSpec.scala
@@ -0,0 +1,17 @@
+package de.htwg.se.gladiators.model.model.model
+
+import org.scalatest.{Matchers, WordSpec}
+
+class CellSpec extends WordSpec with Matchers {
+    "A Cell" when {
+        "new" should {
+            val cell = Cell(CellType.PALM)
+            "have a type" in {
+                cell.cellType should be(CellType.PALM)
+            }
+            "have a nice String representation" in {
+                cell.toString should be("1")
+            }
+        }
+    }
+}
