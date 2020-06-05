200773fb7bce30baf65d8941ecf8b6b06a7abae8
Sebastian Voigt <sebastian.m.voigt@gmail.com>
2020-06-05 11:38:38 +0200

    Rename JsonSupport to PlayerJsonSupport

--- Player/src/main/scala/de/htwg/se/gladiators/util/PlayerJsonSupport.scala
+++ Player/src/main/scala/de/htwg/se/gladiators/util/PlayerJsonSupport.scala
@@ -6,7 +6,7 @@ import akka.http.scaladsl.server.Directives
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import spray.json._
 
-trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
+trait PlayerJsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val playerFormat = jsonFormat5(Player.apply)
   implicit val updateNameArgumentContainerFormat = jsonFormat2(UpdateNameArgumentContainer.apply)
   implicit val baseAttackedArgumentContainerFormat = jsonFormat2(BaseAttackedArgumentContainer.apply)
