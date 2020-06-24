59c81b8d7e9e41ab57ae295b1b2fb2c1190fccc6
Sebastian Voigt <sebastian.m.voigt@gmail.com>
2020-06-24 16:26:01 +0200

    Remove unnecessary prints

--- src/test/scala/de/htwg/se/gladiators/model/ShopSpec.scala
+++ src/test/scala/de/htwg/se/gladiators/model/ShopSpec.scala
@@ -4,13 +4,17 @@ import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers
 
 class ShopSpec extends AnyWordSpec with Matchers {
-    "A de.htwg.se.gladiators.model.Shop when " when {
+    "A de.htwg.se.gladiators.model.Shop " when {
         val shop = new Shop(10)
         "initialized with 10 Gladiators" in {
             shop.stock.size should be(10)
         }
-        "have a nice string representation " in {
-            println(shop.toString)
+        "asking for a string representation " in {
+            shop.toString should startWith("Units available in the shop:")
+            shop.toString should include("AttackPoints")
+            shop.toString should include("MovementPoints")
+            shop.toString should include("Health Points")
+            shop.toString should include("Cost")
         }
     }
 }
