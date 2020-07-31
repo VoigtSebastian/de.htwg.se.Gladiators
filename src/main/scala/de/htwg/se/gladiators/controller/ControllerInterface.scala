72aa75478d21384e04f684ab89481c09e21dacb4
Sebastian Voigt <sebastian.m.voigt@gmail.com>
2020-07-31 20:34:01 +0200

    Fix test

--- src/main/scala/de/htwg/se/gladiators/controller/ControllerInterface.scala
+++ src/main/scala/de/htwg/se/gladiators/controller/ControllerInterface.scala
@@ -3,6 +3,6 @@ package de.htwg.se.gladiators.controller
 import de.htwg.se.gladiators.util.Command
 
 trait ControllerInterface {
-    var gameState = GameState.NamingPlayerOne
+    var gameState: GameState = GameState.NamingPlayerOne
     def inputCommand(command: Command): Unit
 }
