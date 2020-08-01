ea8d5781750425762cb914f69915bf932f0eac3c
Sebastian Voigt <sebastian.m.voigt@gmail.com>
2020-08-01 21:02:56 +0200

    Add tests

---
+++ src/test/scala/de/htwg/se/gladiators/model/BoardSpec.scala
@@ -0,0 +1,34 @@
+package de.htwg.se.gladiators.model
+
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers
+
+import de.htwg.se.gladiators.util.Coordinate
+
+class CoordinateSpec extends AnyWordSpec with Matchers {
+    "A Board" when {
+        "checking valid coordinates" should {
+            "return all coordinates" in {
+                val result = createSandBoard
+                    .getValidCoordinates(
+                        Coordinate(0, 0),
+                        6,
+                        Vector(CellType.Sand)
+                    )
+
+                for ((x, y) <- (0 to 2).map(i => (i, i))) {
+                    result should contain(Coordinate(y, x))
+                }
+            }
+        }
+    }
+
+    def createSandBoard: Board = {
+        val cells = Vector(
+            Vector(CellType.Sand, CellType.Sand, CellType.Sand),
+            Vector(CellType.Sand, CellType.Sand, CellType.Sand),
+            Vector(CellType.Sand, CellType.Sand, CellType.Sand)
+        )
+        Board(cells)
+    }
+}
