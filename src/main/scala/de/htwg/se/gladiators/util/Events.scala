9824d1accd1778f73ef765997a0fe9d1bda5ed38
Sebastian Voigt <sebastian.m.voigt@gmail.com>
2020-08-01 11:40:56 +0200

    Fix typos

--- src/main/scala/de/htwg/se/gladiators/util/Events.scala
+++ src/main/scala/de/htwg/se/gladiators/util/Events.scala
@@ -10,9 +10,9 @@ sealed trait Events extends EnumEntry with Event
 object Events extends Enum[Events] {
     val values = findValues
 
-    case object Init extends Event
+    case object Init extends Events
     case class PlayerOneNamed(name: String) extends Events
     case class PlayerTwoNamed(name: String) extends Events
-    case class Turn(player: Player) extends Event
-    case class ErrorMessage(message: String) extends Event
+    case class Turn(player: Player) extends Events
+    case class ErrorMessage(message: String) extends Events
 }
