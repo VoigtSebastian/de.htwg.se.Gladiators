b73c17e4166b797b98783a99c7bcfba78f1f7542
Sebastian Voigt <sebastian.m.voigt@gmail.com>
2020-08-01 21:15:37 +0200

    Fix missing arguments

--- src/test/scala/de/htwg/se/gladiators/controller/ControllerSpec.scala
+++ src/test/scala/de/htwg/se/gladiators/controller/ControllerSpec.scala
@@ -10,13 +10,13 @@ import de.htwg.se.gladiators.util.Command._
 class ControllerSpec extends AnyWordSpec with Matchers {
     "A controller" when {
         "created" should {
-            val controller = Controller()
+            val controller = Controller(15)
             "be in the NamingPlayerOne state" in {
                 controller.gameState should be(GameState.NamingPlayerOne)
             }
         }
         "receiving commands" should {
-            val controller = Controller()
+            val controller = Controller(15)
             val eventQueue = EventQueue(controller)
             "send out an init Event" in {
                 controller.publish(Init)
