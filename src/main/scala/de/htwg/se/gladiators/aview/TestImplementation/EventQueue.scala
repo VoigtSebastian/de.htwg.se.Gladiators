93881ca32161e2ea6374dbf7d1013415467922f3
Sebastian Voigt <sebastian.m.voigt@gmail.com>
2020-08-01 11:52:11 +0200

    Add tests

---
+++ src/main/scala/de/htwg/se/gladiators/aview/TestImplementation/EventQueue.scala
@@ -0,0 +1,20 @@
+package de.htwg.se.gladiators.aview.TestImplementation
+
+import de.htwg.se.gladiators.controller.BaseImplementation.Controller
+import scala.swing.Reactor
+import scala.collection.mutable.Queue
+import de.htwg.se.gladiators.util.Events
+import de.htwg.se.gladiators.util.Events._
+
+case class EventQueue(controller: Controller) extends Reactor {
+    listenTo(controller)
+    val events: Queue[Events] = Queue()
+
+    reactions += {
+        case Init => events.enqueue(Init)
+        case PlayerOneNamed(name) => events.enqueue(PlayerOneNamed(name))
+        case PlayerTwoNamed(name) => events.enqueue(PlayerTwoNamed(name))
+        case Turn(player) => events.enqueue(Turn(player))
+        case ErrorMessage(message) => events.enqueue(ErrorMessage(message))
+    }
+}
---
+++ src/test/scala/de/htwg/se/gladiators/controller/ControllerSpec.scala
@@ -0,0 +1,31 @@
+package de.htwg.se.gladiators.controller
+
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers
+import de.htwg.se.gladiators.controller.BaseImplementation.Controller
+import de.htwg.se.gladiators.aview.TestImplementation.EventQueue
+import de.htwg.se.gladiators.util.Events._
+import de.htwg.se.gladiators.util.Command._
+
+class ControllerSpec extends AnyWordSpec with Matchers {
+    "A controller" when {
+        "created" should {
+            val controller = Controller()
+            "be in the NamingPlayerOne state" in {
+                controller.gameState should be(GameState.NamingPlayerOne)
+            }
+        }
+        "receiving commands" should {
+            val controller = Controller()
+            val eventQueue = EventQueue(controller)
+            "send out an init Event" in {
+                controller.publish(Init)
+                eventQueue.events.dequeue() should be(Init)
+            }
+            "send out an Error message" in {
+                controller.inputCommand(EndTurn)
+                eventQueue.events.dequeue().isInstanceOf[ErrorMessage] should be(true)
+            }
+        }
+    }
+}
