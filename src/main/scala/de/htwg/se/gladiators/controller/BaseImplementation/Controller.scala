7616047314ad31d1ad4c9685c2b5d35b7734c4fc
Sebastian Voigt <sebastian.m.voigt@gmail.com>
2020-08-01 10:41:17 +0200

    Add events

--- src/main/scala/de/htwg/se/gladiators/controller/BaseImplementation/Controller.scala
+++ src/main/scala/de/htwg/se/gladiators/controller/BaseImplementation/Controller.scala
@@ -2,7 +2,33 @@ package de.htwg.se.gladiators.controller.BaseImplementation
 
 import de.htwg.se.gladiators.controller.ControllerInterface
 import de.htwg.se.gladiators.util.Command
+import de.htwg.se.gladiators.util.Command._
+import de.htwg.se.gladiators.util.Events._
+import de.htwg.se.gladiators.controller.GameState._
+import de.htwg.se.gladiators.model.Player
 
 case class Controller() extends ControllerInterface {
-    override def inputCommand(command: Command): Unit = println(command)
+
+    override def inputCommand(command: Command): Unit = {
+        println(command)
+        command match {
+            case NamePlayerOne(name) => {
+                gameState = NamingPlayerTwo
+                publish(PlayerOneNamed(name))
+            }
+            case NamePlayerTwo(name) => {
+                gameState = TurnPlayerOne
+                publish(PlayerTwoNamed(name))
+            }
+            case EndTurn => gameState match {
+                case TurnPlayerOne => publish(Turn(Player("two")))
+                case TurnPlayerTwo => publish(Turn(Player("one")))
+                case _ => publish(ErrorMessage(s"You can not end a turn in gameState $gameState"))
+            }
+            case Attack(from, to) => println(s"Attacking from $from to $to")
+            case BuyUnit(number) => println(s"Buying unit $number")
+            case Move(from, to) => println(s"Moving from $from to $to")
+            case Quit => println("Goodbye")
+        }
+    }
 }
--- src/main/scala/de/htwg/se/gladiators/controller/ControllerInterface.scala
+++ src/main/scala/de/htwg/se/gladiators/controller/ControllerInterface.scala
@@ -1,8 +1,9 @@
 package de.htwg.se.gladiators.controller
 
 import de.htwg.se.gladiators.util.Command
+import scala.swing.Publisher
 
-trait ControllerInterface {
+trait ControllerInterface extends Publisher {
     var gameState: GameState = GameState.NamingPlayerOne
     def inputCommand(command: Command): Unit
 }
