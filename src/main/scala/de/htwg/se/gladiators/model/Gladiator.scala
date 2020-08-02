f2ea69a99d37d11442f2ed10067a94dcdc086a6b
Sebastian Voigt <sebastian.m.voigt@gmail.com>
2020-08-02 11:07:27 +0200

    Add position to Gladiator

--- src/main/scala/de/htwg/se/gladiators/model/Gladiator.scala
+++ src/main/scala/de/htwg/se/gladiators/model/Gladiator.scala
@@ -1,6 +1,9 @@
 package de.htwg.se.gladiators.model
 
+import de.htwg.se.gladiators.util.Coordinate
+
 case class Gladiator(
     gladiatorType: GladiatorType,
+    position: Coordinate,
     healthPoints: Int,
     movementPoints: Int)
