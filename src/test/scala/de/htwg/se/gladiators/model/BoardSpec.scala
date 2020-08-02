5c923876390bce058bd59ce8503915c6c4bc32ef
Sebastian Voigt <sebastian.m.voigt@gmail.com>
2020-08-02 09:58:57 +0200

    Add tests

--- src/test/scala/de/htwg/se/gladiators/model/BoardSpec.scala
+++ src/test/scala/de/htwg/se/gladiators/model/BoardSpec.scala
@@ -19,6 +19,65 @@ class CoordinateSpec extends AnyWordSpec with Matchers {
                     result should contain(Coordinate(y, x))
                 }
             }
+            "return no coordinates" in {
+                val result = createSandBoard
+                    .getValidCoordinates(
+                        Coordinate(0, 0),
+                        6,
+                        Vector(CellType.Palm))
+
+                result should be(empty)
+            }
+            "return only walkable coordinates" in {
+                val result = createNormalBoard
+                    .getValidCoordinates(
+                        Coordinate(1, 1),
+                        6,
+                        Vector(CellType.Sand))
+
+                for (x <- (0 to 2)) {
+                    result should contain(Coordinate(x, 1))
+                }
+                result should contain(Coordinate(0, 0))
+                result should contain(Coordinate(2, 2))
+            }
+            "return only coordinates that can be attacked" in {
+                val result = createNormalBoard
+                    .getValidCoordinates(
+                        Coordinate(1, 1),
+                        6,
+                        Vector(CellType.Sand, CellType.Base))
+
+                for (x <- (0 to 2)) {
+                    result should contain(Coordinate(x, 1))
+                }
+
+                result should contain(Coordinate(0, 0))
+                result should contain(Coordinate(0, 1))
+                result should contain(Coordinate(2, 1))
+                result should contain(Coordinate(2, 2))
+            }
+        }
+        "checking if coordinates are legal" should {
+            val board = createSandBoard
+            "return true" in {
+                for ((x, y) <- (0 to 2).map(i => (i, i))) {
+                    board.isCoordinateLegal(Coordinate(x, y)) should be(true)
+                }
+            }
+            "return false" in {
+                for ((x, y) <- (3 to 5).map(i => (i, i)) ++ (-3 to -1).map(i => (i, i))) {
+                    board.isCoordinateLegal(Coordinate(x, y)) should be(false)
+                }
+            }
+        }
+        "being asked for a Cell" should {
+            "return Sand" in {
+                val board = createSandBoard
+                for ((x, y) <- (0 to 2).map(i => (i, i))) {
+                    board.cellAtCoordinate(Coordinate(0, 2)) should be(CellType.Sand)
+                }
+            }
         }
     }
 
@@ -27,6 +86,14 @@ class CoordinateSpec extends AnyWordSpec with Matchers {
             Vector(CellType.Sand, CellType.Sand, CellType.Sand),
             Vector(CellType.Sand, CellType.Sand, CellType.Sand),
             Vector(CellType.Sand, CellType.Sand, CellType.Sand))
+        Board(cells)
+    }
+
+    def createNormalBoard: Board = {
+        val cells = Vector(
+            Vector(CellType.Sand, CellType.Base, CellType.Palm),
+            Vector(CellType.Sand, CellType.Sand, CellType.Sand),
+            Vector(CellType.Palm, CellType.Base, CellType.Sand))
         Board(cells)
     }
 }
