d13004098aedbde1db68b223608fcf965395d557
Sebastian Voigt <sebastian.m.voigt@gmail.com>
2020-08-03 14:44:56 +0200

    Switch to JFXApp

--- src/main/scala/de/htwg/se/gladiators/Gladiators.scala
+++ src/main/scala/de/htwg/se/gladiators/Gladiators.scala
@@ -4,15 +4,16 @@ import de.htwg.se.gladiators.aview.Tui
 import de.htwg.se.gladiators.controller.BaseImplementation.Controller
 import de.htwg.se.gladiators.util.Events.Init
 import de.htwg.se.gladiators.aview.FxGui.Gui
-import javafx.embed.swing.JFXPanel
-import javafx.application.Platform
+import scala.concurrent.{ Future, blocking }
+import scala.concurrent.ExecutionContext
 
 object Main extends App {
-    val controller = Controller(15)
-    val tui = Tui(controller)
+    implicit val ec = ExecutionContext.global
 
-    new JFXPanel()
-    Platform.runLater(new Runnable { def run() = Gui(controller).show() })
+    val controller = Controller(15)
+
+    val tui = Tui(controller)
+    Future { blocking { Gui(controller).main(args) } }
 
     controller.publish(Init)
     while (tui.processInputLine(scala.io.StdIn.readLine())) {}
--- src/main/scala/de/htwg/se/gladiators/aview/FxGui/Gui.scala
+++ src/main/scala/de/htwg/se/gladiators/aview/FxGui/Gui.scala
@@ -8,37 +8,39 @@ import scalafx.scene.paint.Color._
 import scalafx.scene.paint.{ Stops, LinearGradient }
 import scalafx.scene.text.Text
 import de.htwg.se.gladiators.controller.ControllerInterface
-import scalafx.stage.Stage
 import scalafx.stage.StageStyle
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.application.JFXApp
 
-case class Gui(controller: ControllerInterface) extends Stage {
-    title = "ScalaFX Hello World"
-    initStyle(StageStyle.Decorated)
-    // override def style: StageStyle = StageStyle.Decorated
-    scene = new Scene {
-        fill = Black
-        content = new HBox {
-            padding = Insets(20)
-            children = Seq(
-                new Text {
-                    text = "Hello "
-                    style = "-fx-font-size: 48pt"
-                    fill = new LinearGradient(
-                        endX = 0,
-                        stops = Stops(PaleGreen, SeaGreen))
-                },
-                new Text {
-                    text = "World!!!"
-                    style = "-fx-font-size: 48pt"
-                    fill = new LinearGradient(
-                        endX = 0,
-                        stops = Stops(Cyan, DodgerBlue))
-                    effect = new DropShadow {
-                        color = DodgerBlue
-                        radius = 25
-                        spread = 0.25
-                    }
-                })
+case class Gui(controller: ControllerInterface) extends JFXApp {
+    stage = new PrimaryStage {
+        title = "ScalaFX Hello World"
+        initStyle(StageStyle.Decorated)
+        scene = new Scene {
+            fill = Black
+            content = new HBox {
+                padding = Insets(20)
+                children = Seq(
+                    new Text {
+                        text = "Hello "
+                        style = "-fx-font-size: 48pt"
+                        fill = new LinearGradient(
+                            endX = 0,
+                            stops = Stops(PaleGreen, SeaGreen))
+                    },
+                    new Text {
+                        text = "World!!!"
+                        style = "-fx-font-size: 48pt"
+                        fill = new LinearGradient(
+                            endX = 0,
+                            stops = Stops(Cyan, DodgerBlue))
+                        effect = new DropShadow {
+                            color = DodgerBlue
+                            radius = 25
+                            spread = 0.25
+                        }
+                    })
+            }
         }
     }
 }
