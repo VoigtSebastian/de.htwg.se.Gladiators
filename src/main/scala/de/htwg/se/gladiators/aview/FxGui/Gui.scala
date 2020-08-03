e192210714e3b6291e6f8956a41f207f449c0ac3
Sebastian Voigt <sebastian.m.voigt@gmail.com>
2020-08-03 23:02:35 +0200

    Add textures and callback

--- src/main/scala/de/htwg/se/gladiators/aview/FxGui/Gui.scala
+++ src/main/scala/de/htwg/se/gladiators/aview/FxGui/Gui.scala
@@ -1,14 +1,12 @@
 package de.htwg.se.gladiators.aview.FxGui
 
 import scalafx.scene.Scene
-import scalafx.scene.paint.Color._
 import de.htwg.se.gladiators.controller.ControllerInterface
 import scalafx.stage.StageStyle
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.application.JFXApp
-import scalafx.scene.control.Button
 import scalafx.scene.layout.GridPane
-import scalafx.geometry.Insets
+import de.htwg.se.gladiators.model.TileType.Sand
 
 case class Gui(controller: ControllerInterface) extends JFXApp {
     stage = new PrimaryStage {
@@ -17,19 +15,14 @@ case class Gui(controller: ControllerInterface) extends JFXApp {
         fullScreen_=(true)
         scene = new Scene {
             val pane = new GridPane {
-                padding = Insets(10)
-                margin = Insets(10)
+                // padding = Insets(10)
+                // margin = Insets(10)
             }
             (1 to 15).map(x => (1 to 15).map(y =>
-                pane.add(
-                    new Button {
-                        text = s"x$x,y$y"
-                        onMousePressed = (_) => { println(s"x$x,y$y") }
-                        padding = Insets(5)
-                        margin = Insets(5)
-                    },
-                    x, y)))
+                pane.add(Sand.toButton(x, y, callOnButtonPressed), x, y)))
             content = pane
         }
     }
+
+    def callOnButtonPressed(x: Int, y: Int) = println(s"x$x,y$y")
 }
--- src/main/scala/de/htwg/se/gladiators/model/TileType.scala
+++ src/main/scala/de/htwg/se/gladiators/model/TileType.scala
@@ -1,8 +1,26 @@
 package de.htwg.se.gladiators.model
 
 import enumeratum._
+import scalafx.scene.control.Button
+import javafx.scene.control
+import scalafx.scene.image.ImageView
+import java.io.File
+import scalafx.geometry.Insets
 
-sealed trait TileType extends EnumEntry
+sealed trait TileType extends EnumEntry {
+    // val image = new ImageView(new File("textures/sand_60.png").toURI.toString)
+    def toButton(x: Int, y: Int, call: (Int, Int) => Unit): Button = new Button {
+        onMousePressed = (event) => {
+            println(event.getSource.asInstanceOf[control.Button])
+            call(x, y)
+        }
+        graphic = new ImageView(new File("textures/sand_60.png").toURI.toString)
+        prefHeight = 60
+        prefWidth = 60
+        padding = Insets(0)
+        margin = Insets(0)
+    }
+}
 
 object TileType extends Enum[TileType] {
     val values = findValues
