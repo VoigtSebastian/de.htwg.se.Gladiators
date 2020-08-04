87ef0c94a9e4eaeef4b716742e4da4cdff3ad9ce
Sebastian Voigt <sebastian.m.voigt@gmail.com>
2020-08-04 21:30:35 +0200

    Create BoardFactorySpec

---
+++ src/test/scala/de/htwg/se/gladiators/util/Factories/BoardFactorySpec.scala
@@ -0,0 +1,39 @@
+package de.htwg.se.gladiators.util.Factories
+
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers
+
+import de.htwg.se.gladiators.model.Board
+import de.htwg.se.gladiators.model.TileType
+
+class BoardFactorySpec extends AnyWordSpec with Matchers {
+    "A BoardFactory" when {
+        "creating a board" should {
+            "add a base to an existing board" in {
+                val board = BoardFactory
+                    .addBase(createSandBoard.tiles, createSandBoard.tiles.length)
+                board(0)(1) should be(TileType.Base)
+                board(2)(1) should be(TileType.Base)
+            }
+            "add a mine to an existing board" in {
+                val board = BoardFactory
+                    .addMine(createSandBoard.tiles, createSandBoard.tiles.length)
+                board(1).filter(_.isInstanceOf[TileType.Mine]).length > 0 should be(true)
+            }
+            "combine everything into one" in {
+                val board = BoardFactory.initRandomBoard(3)
+                board.tiles(0)(1) should be(TileType.Base)
+                board.tiles(2)(1) should be(TileType.Base)
+                board.tiles(1).filter(_.isInstanceOf[TileType.Mine]).length > 0 should be(true)
+            }
+        }
+    }
+
+    def createSandBoard: Board = {
+        val tiles = Vector(
+            Vector(TileType.Sand, TileType.Sand, TileType.Sand),
+            Vector(TileType.Sand, TileType.Sand, TileType.Sand),
+            Vector(TileType.Sand, TileType.Sand, TileType.Sand))
+        Board(tiles)
+    }
+}
