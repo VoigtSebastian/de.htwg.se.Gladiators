9525de39a8a80af956794087e664cdc0c810d810
Sebastian Voigt <sebastian.m.voigt@gmail.com>
2020-08-04 20:45:50 +0200

    Add colored string functions

--- src/main/scala/de/htwg/se/gladiators/model/TileType.scala
+++ src/main/scala/de/htwg/se/gladiators/model/TileType.scala
@@ -2,13 +2,25 @@ package de.htwg.se.gladiators.model
 
 import enumeratum._
 
-sealed trait TileType extends EnumEntry
+sealed trait TileType extends EnumEntry {
+    def coloredStringRepresentation(gladiator: Option[GladiatorType]): String
+}
 
 object TileType extends Enum[TileType] {
     val values = findValues
 
-    case object Sand extends TileType
-    case object Palm extends TileType
-    case object Base extends TileType
-    case class Mine(gold: Int) extends TileType
+    case object Sand extends TileType {
+        override def coloredStringRepresentation(gladiator: Option[GladiatorType]): String = coloredString(220, gladiator, "S")
+    }
+    case object Palm extends TileType {
+        override def coloredStringRepresentation(gladiator: Option[GladiatorType]): String = coloredString(154, gladiator, "P")
+    }
+    case object Base extends TileType {
+        override def coloredStringRepresentation(gladiator: Option[GladiatorType]): String = coloredString(203, gladiator, "B")
+    }
+    case class Mine(gold: Int) extends TileType {
+        override def coloredStringRepresentation(gladiator: Option[GladiatorType]): String = coloredString(223, gladiator, "M")
+    }
+
+    def coloredString(color: Int, gladiator: Option[GladiatorType], letter: String) = s"\u001b[48;5;${color}m" + gladiator.getOrElse(s" $letter ") + "\u001b[0m"
 }
