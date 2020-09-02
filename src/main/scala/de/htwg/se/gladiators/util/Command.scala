8263e33c140d839cd20db21ac570c0fe8728a2bd
VoigtSebastian <VoigtSebastian@protonmail.com>
2020-09-02 19:21:28 +0200

    Remove Attack command

--- src/main/scala/de/htwg/se/gladiators/util/Command.scala
+++ src/main/scala/de/htwg/se/gladiators/util/Command.scala
@@ -8,7 +8,6 @@ object Command extends Enum[Command] {
     val values = findValues
 
     case class Move(from: Coordinate, to: Coordinate) extends Command
-    case class Attack(from: Coordinate, to: Coordinate) extends Command
     case class BuyUnit(number: Int) extends Command
     case class NamePlayerOne(name: String) extends Command
     case class NamePlayerTwo(name: String) extends Command
