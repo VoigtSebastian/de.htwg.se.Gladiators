ade6d3491a108f9958e29887d18863584b3611be
VoigtSebastian@protonmail.com <sebastian@DESKTOP-4R00RD2.localdomain>
2020-09-05 21:53:07 +0200

    Simplify EventQueue

--- src/main/scala/de/htwg/se/gladiators/aview/TestImplementation/EventQueue.scala
+++ src/main/scala/de/htwg/se/gladiators/aview/TestImplementation/EventQueue.scala
@@ -3,19 +3,11 @@ package de.htwg.se.gladiators.aview.TestImplementation
 import scala.swing.Reactor
 import scala.collection.mutable.Queue
 import de.htwg.se.gladiators.util.Events
-import de.htwg.se.gladiators.util.Events._
 import de.htwg.se.gladiators.controller.ControllerInterface
 
 case class EventQueue(controller: ControllerInterface) extends Reactor {
     listenTo(controller)
     val events: Queue[Events] = Queue()
 
-    reactions += {
-        case Init => events.enqueue(Init)
-        case PlayerOneNamed(name) => events.enqueue(PlayerOneNamed(name))
-        case PlayerTwoNamed(name) => events.enqueue(PlayerTwoNamed(name))
-        case Turn(player) => events.enqueue(Turn(player))
-        case ErrorMessage(message) => events.enqueue(ErrorMessage(message))
-        case SuccessfullyBoughtGladiator(player, gladiator) => events.enqueue(SuccessfullyBoughtGladiator(player, gladiator))
-    }
+    reactions += { case e: Events => events.enqueue(e) }
 }
