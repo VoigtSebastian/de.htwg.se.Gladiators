9e86697a29987ffe0801c26af99590ec0317b1d8
VoigtSebastian <VoigtSebastian@protonmail.com>
2020-09-16 21:35:10 +0200

    Add missing test

--- src/test/scala/de/htwg/se/gladiators/model/PlayerSpec.scala
+++ src/test/scala/de/htwg/se/gladiators/model/PlayerSpec.scala
@@ -4,6 +4,7 @@ import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers
 import de.htwg.se.gladiators.util.Factories.BoardFactory
 import de.htwg.se.gladiators.util.Coordinate
+import de.htwg.se.gladiators.util.Factories.GladiatorFactory
 
 class PlayerSpec extends AnyWordSpec with Matchers {
     "A Player" should {
@@ -26,10 +27,19 @@ class PlayerSpec extends AnyWordSpec with Matchers {
             tiles should contain(Coordinate(4, 6))
             tiles should contain(Coordinate(3, 5))
         }
-        "return no tiles" in {
+        "return no tiles because they have the wrong type" in {
             val palmBoard = BoardFactory.initRandomBoard(7, 0)
             playerOne.placementTilesNewUnit(7, palmBoard.tiles) should be(empty)
             playerTwo.placementTilesNewUnit(7, palmBoard.tiles) should be(empty)
+        }
+        "return no tiles because they are blocked" in {
+            playerOne
+                .copy(gladiators = Vector(
+                    GladiatorFactory.createGladiator(position = Some(Coordinate(2, 0))),
+                    GladiatorFactory.createGladiator(position = Some(Coordinate(4, 0))),
+                    GladiatorFactory.createGladiator(position = Some(Coordinate(3, 1)))
+                ))
+                .placementTilesNewUnit(7, board.tiles) should be(empty)
         }
     }
 }
