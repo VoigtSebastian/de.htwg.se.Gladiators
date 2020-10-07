90c169d96609ce2e72470b0bab668b5767ecb85e
VoigtSebastian@protonmail.com <sebastian@DESKTOP-4R00RD2.localdomain>
2020-10-07 17:13:46 +0200

    Add Events

--- src/main/scala/de/htwg/se/gladiators/util/Events.scala
+++ src/main/scala/de/htwg/se/gladiators/util/Events.scala
@@ -17,6 +17,8 @@ object Events extends Enum[Events] {
     case class Turn(player: Player) extends Events
     case class Moved(player: Player, from: Coordinate, to: Coordinate, gladiator: Gladiator) extends Events
     case class Attacked(currentPlayer: Player, killed: Boolean) extends Events
+    case class BaseAttacked(currentPlayer: Player) extends Events
     case object Shutdown extends Events
+    case class Won(player: Player) extends Events
     case class ErrorMessage(message: String) extends Events
 }
