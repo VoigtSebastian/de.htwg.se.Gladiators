7d675256efae7e251283ec924ea73a849ff5dfbe
VoigtSebastian@protonmail.com <sebastian@DESKTOP-4R00RD2.localdomain>
2020-10-08 16:45:32 +0200

    Add Mined event

--- src/main/scala/de/htwg/se/gladiators/util/Events.scala
+++ src/main/scala/de/htwg/se/gladiators/util/Events.scala
@@ -18,6 +18,7 @@ object Events extends Enum[Events] {
     case class Moved(player: Player, from: Coordinate, to: Coordinate, gladiator: Gladiator) extends Events
     case class Attacked(currentPlayer: Player, killed: Boolean, from: Coordinate, to: Coordinate) extends Events
     case class BaseAttacked(currentPlayer: Player) extends Events
+    case class Mined(currentPlayer: Player, amount: Int, depleted: Boolean) extends Events
     case object Shutdown extends Events
     case class Won(player: Player) extends Events
     case class ErrorMessage(message: String) extends Events
