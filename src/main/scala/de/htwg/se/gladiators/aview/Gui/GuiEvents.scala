9d2280de0d40412ee2cffe31f314a62e196e8bbe
VoigtSebastian <VoigtSebastian@protonmail.com>
2020-10-10 17:01:09 +0200

    Add TileClicked GuiEvent

--- src/main/scala/de/htwg/se/gladiators/aview/Gui/GuiEvents.scala
+++ src/main/scala/de/htwg/se/gladiators/aview/Gui/GuiEvents.scala
@@ -2,11 +2,13 @@ package de.htwg.se.gladiators.aview.Gui
 
 import scala.swing.event.Event
 import enumeratum._
+import de.htwg.se.gladiators.util.Coordinate
 
-sealed trait Events extends EnumEntry with Event
+sealed trait GuiEvents extends EnumEntry with Event
 
-object GuiEvents extends Enum[Events] {
+object GuiEvents extends Enum[GuiEvents] {
     val values = findValues
 
-    case class ShopClicked(item: Int) extends Events
+    case class ShopClicked(item: Int) extends GuiEvents
+    case class TileClicked(coordinate: Coordinate) extends GuiEvents
 }
