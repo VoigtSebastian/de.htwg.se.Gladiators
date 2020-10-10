83d7dbc0d2b1783baa77190967508eb2962350fa
VoigtSebastian@protonmail.com <sebastian@DESKTOP-4R00RD2.localdomain>
2020-10-10 17:04:41 +0200

    Add reactor to Gui

--- src/main/scala/de/htwg/se/gladiators/aview/Gui/Gui.scala
+++ src/main/scala/de/htwg/se/gladiators/aview/Gui/Gui.scala
@@ -6,20 +6,47 @@ import de.htwg.se.gladiators.util.Command.Quit
 import de.htwg.se.gladiators.util.Configuration
 import scala.swing.event.WindowClosing
 import scala.swing._
+import de.htwg.se.gladiators.util.Coordinate
 
-class Gui(controller: ControllerInterface, configuration: Configuration) extends MainFrame {
+class Gui(controller: ControllerInterface, configuration: Configuration) extends MainFrame with Reactor {
     listenTo(controller)
     title = "Gladiator"
     var selectedShopItem: Option[Int] = None
+    var selectedTile: Option[Coordinate] = None
+
+    implicit class BetterComponents(component: Component) {
+        def listenAndReturn = {
+            listenTo(component)
+            component
+        }
+    }
 
     contents = new BoxPanel(Orientation.Horizontal) {
-        contents += listenToContents(ShopPanel(configuration.itemsInShop))
-        contents += Button("Press me, please") { println("Thank you") }
+        contents += ShopPanel(configuration.itemsInShop).listenAndReturn
+        contents += BoardPanel(configuration.boardSize).listenAndReturn
     }
 
     reactions += {
-        case ShopClicked(number) => selectedShopItem = Some(number)
         case WindowClosing(_) => controller.inputCommand(Quit)
+
+        case ShopClicked(number) => {
+            selectedShopItem = Some(number)
+            println(f"Shop clicked $number")
+        }
+
+        case TileClicked(newTile) => (selectedTile, selectedShopItem) match {
+            case (None, None) => { selectedTile = Some(newTile) }
+            case (Some(_), None) if selectedTile.get != newTile => {
+                move(newTile)
+                resetSelected
+            }
+            case (Some(_), None) => selectedTile = None
+            case (None, Some(_)) => {
+                println(f"Buy unit ${selectedShopItem.get} to ${newTile}")
+                resetSelected
+            }
+            case (Some(_), Some(_)) => ()
+        }
     }
 
     peer.setDefaultCloseOperation(0)
@@ -27,8 +54,10 @@ class Gui(controller: ControllerInterface, configuration: Configuration) extends
     pack
     visible = true
 
-    def listenToContents(component: Component) = {
-        listenTo(component)
-        component
+    def resetSelected = {
+        selectedTile = None
+        selectedShopItem = None
     }
+
+    def move(to: Coordinate) = println(f"Move from ${selectedTile.get} to $to")
 }
