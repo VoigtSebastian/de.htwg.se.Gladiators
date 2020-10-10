dc2c5ae903816a4a54e8b2e2da429daa185d3c58
VoigtSebastian <VoigtSebastian@protonmail.com>
2020-10-10 19:04:19 +0200

    Add selection and deselection to BoardPanel

--- src/main/scala/de/htwg/se/gladiators/aview/Gui/BoardPanel.scala
+++ src/main/scala/de/htwg/se/gladiators/aview/Gui/BoardPanel.scala
@@ -1,11 +1,27 @@
 package de.htwg.se.gladiators.aview.Gui
 
-import scala.swing.GridPanel
-import scala.swing.{ Button, Publisher }
 import de.htwg.se.gladiators.aview.Gui.GuiEvents.TileClicked
 import de.htwg.se.gladiators.util.Coordinate
+import java.awt.Color
+import scala.swing._
 
 case class BoardPanel(length: Int) extends GridPanel(length, length) with Publisher {
-    (1 to length)
-        .foreach(y => (y, (1 to length).foreach(x => contents += Button(f"$x $y") { publish(TileClicked(Coordinate(x, y))) })))
+    val tiles = (0 to (length - 1))
+        .map(y => (0 to (length - 1)).map(x => new Label(f"$x $y") {
+            listenTo(mouse.clicks)
+            deselect
+            reactions += { case _: event.MouseClicked => tileClicked(Coordinate(x, y)) }
+
+            def select = (border = Swing.LineBorder(Color.red, 4))
+            def deselect = (border = Swing.LineBorder(Color.black, 2))
+        }).toVector)
+        .toVector
+
+    tiles.foreach(_.foreach(contents += _))
+
+    def tileClicked(tile: Coordinate) = publish(TileClicked(Coordinate(tile.x, tile.y)))
+
+    def selectTile(coordinate: Coordinate) = tiles(coordinate.y)(coordinate.x).select
+    def deselectTile(coordinate: Coordinate) = tiles(coordinate.y)(coordinate.x).deselect
+
 }
