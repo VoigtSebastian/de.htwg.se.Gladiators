06c1b9f19ce7935bf83217783211ca0e810f00c1
VoigtSebastian@protonmail.com <sebastian@DESKTOP-4R00RD2.localdomain>
2020-10-11 20:51:58 +0200

    Add Gladiator Components

---
+++ src/main/scala/de/htwg/se/gladiators/aview/Gui/GladiatorComponents/GButton.scala
@@ -0,0 +1,13 @@
+package de.htwg.se.gladiators.aview.Gui.GladiatorComponents
+
+import scala.swing._
+import de.htwg.se.gladiators.aview.Gui.GladiatorComponents.GCommon._
+
+object GButton {
+    def apply(text: String)(op: => Unit) = {
+        val button = Button(text)(op)
+        button.font = commonFont
+        button.border = commonBorder
+        button
+    }
+}
---
+++ src/main/scala/de/htwg/se/gladiators/aview/Gui/GladiatorComponents/GCommon.scala
@@ -0,0 +1,9 @@
+package de.htwg.se.gladiators.aview.Gui.GladiatorComponents
+
+import scala.swing._
+import javax.swing.border.EmptyBorder
+
+object GCommon {
+    def commonFont = new Font("Algeria", Font.Bold.id, 16)
+    def commonBorder = new EmptyBorder(10, 10, 10, 10)
+}
