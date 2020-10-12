476d6c0faa4024a588704d759455478ed3473f05
VoigtSebastian@protonmail.com <sebastian@DESKTOP-4R00RD2.localdomain>
2020-10-12 12:38:15 +0200

    Add icons to BoardLabel

--- src/main/scala/de/htwg/se/gladiators/aview/Gui/GladiatorComponents/Board/BoardPanel.scala
+++ src/main/scala/de/htwg/se/gladiators/aview/Gui/GladiatorComponents/Board/BoardPanel.scala
@@ -4,15 +4,16 @@ import de.htwg.se.gladiators.aview.Gui.GuiEvents.TileClicked
 import de.htwg.se.gladiators.util.Coordinate
 import scala.swing._
 import de.htwg.se.gladiators.aview.Gui.GladiatorComponents.Board.BoardLabel
+import de.htwg.se.gladiators.model.TileType
 
 case class BoardPanel(length: Int) extends GridPanel(length, length) with Publisher {
     val tiles = (0 to (length - 1))
-        .map(y => (0 to (length - 1)).map(x => BoardLabel(x)(y)(tileClicked)).toVector)
+        .map(y => (0 to (length - 1)).map(x => new BoardLabel(x, y, TileType.Sand)(tileClicked)).toVector)
         .toVector
 
     tiles.foreach(_.foreach(contents += _))
 
-    def tileClicked(tile: Coordinate) = publish(TileClicked(Coordinate(tile.x, tile.y)))
+    def tileClicked(tile: Coordinate) = publish(TileClicked(tile))
 
     def selectTile(coordinate: Coordinate) = tiles(coordinate.y)(coordinate.x).select
     def deselectTile(coordinate: Coordinate) = tiles(coordinate.y)(coordinate.x).deselect
