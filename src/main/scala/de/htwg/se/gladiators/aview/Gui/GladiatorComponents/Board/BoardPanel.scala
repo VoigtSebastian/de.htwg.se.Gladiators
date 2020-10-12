02d7e96f99324b1638b33092ebc96b1030c316a1
VoigtSebastian <VoigtSebastian@protonmail.com>
2020-10-12 11:31:40 +0200

    Move BoardPanel

--- src/main/scala/de/htwg/se/gladiators/aview/Gui/GladiatorComponents/Board/BoardPanel.scala
+++ src/main/scala/de/htwg/se/gladiators/aview/Gui/GladiatorComponents/Board/BoardPanel.scala
@@ -2,19 +2,12 @@ package de.htwg.se.gladiators.aview.Gui
 
 import de.htwg.se.gladiators.aview.Gui.GuiEvents.TileClicked
 import de.htwg.se.gladiators.util.Coordinate
-import java.awt.Color
 import scala.swing._
+import de.htwg.se.gladiators.aview.Gui.GladiatorComponents.Board.BoardLabel
 
 case class BoardPanel(length: Int) extends GridPanel(length, length) with Publisher {
     val tiles = (0 to (length - 1))
-        .map(y => (0 to (length - 1)).map(x => new Label(f"$x $y") {
-            listenTo(mouse.clicks)
-            deselect
-            reactions += { case _: event.MouseClicked => tileClicked(Coordinate(x, y)) }
-
-            def select = (border = Swing.LineBorder(Color.red, 4))
-            def deselect = (border = Swing.LineBorder(Color.black, 2))
-        }).toVector)
+        .map(y => (0 to (length - 1)).map(x => BoardLabel(x)(y)(tileClicked)).toVector)
         .toVector
 
     tiles.foreach(_.foreach(contents += _))
