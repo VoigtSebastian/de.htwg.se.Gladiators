f8a1ad7beba1a82e4fd950eb96c1bdfc8cfb735a
VoigtSebastian <VoigtSebastian@protonmail.com>
2020-10-12 12:38:15 +0200

    Add icons to BoardLabel

--- src/main/scala/de/htwg/se/gladiators/aview/Gui/GladiatorComponents/Board/BoardLabel.scala
+++ src/main/scala/de/htwg/se/gladiators/aview/Gui/GladiatorComponents/Board/BoardLabel.scala
@@ -2,15 +2,35 @@ package de.htwg.se.gladiators.aview.Gui.GladiatorComponents.Board
 
 import de.htwg.se.gladiators.util.Coordinate
 import scala.swing._
+import javax.swing.ImageIcon
 import java.awt.Color
+import de.htwg.se.gladiators.model.TileType
+import de.htwg.se.gladiators.model.TileType._
 
-object BoardLabel {
-    def apply(x: Int)(y: Int)(onClick: Coordinate => Unit) = new Label(f"$x $y") {
-        font = new Font("Algeria", Font.Plain.id, 14)
-        listenTo(mouse.clicks)
-        deselect
-        reactions += { case _: event.MouseClicked => onClick(Coordinate(x, y)) }
-        def select = (border = Swing.LineBorder(Color.red, 4))
-        def deselect = (border = Swing.LineBorder(Color.black, 2))
+class BoardLabel(x: Int, y: Int, tileType: TileType)(onClick: Coordinate => Unit) extends Label(f"$x $y") {
+    font = new Font("Algeria", Font.Plain.id, 14)
+    listenTo(mouse.clicks)
+    deselect
+    reactions += { case _: event.MouseClicked => onClick(Coordinate(x, y)) }
+    horizontalTextPosition = Alignment.Center
+
+    changeTileType(tileType)
+
+    def select = (border = Swing.LineBorder(Color.red, 2))
+    def deselect = (border = Swing.LineBorder(Color.black, 2))
+
+    def changeTileType(newTileType: TileType) = {
+        newTileType match {
+            case Base => icon = resizedTexture("textures/housesand_small.png", 40, 40)
+            case Sand => icon = resizedTexture("textures/sand_60.png", 40, 40)
+            case Palm => icon = resizedTexture("textures/palmsand_small_color.png", 40, 40)
+            case Mine(_) => icon = resizedTexture("textures/sand_60.png", 40, 40)
+        }
+    }
+    def resizedTexture(path: String, width: Int, height: Int): ImageIcon = {
+        val image = new ImageIcon(path)
+            .getImage
+            .getScaledInstance(width, height, java.awt.Image.SCALE_SMOOTH)
+        new ImageIcon(image);
     }
 }
