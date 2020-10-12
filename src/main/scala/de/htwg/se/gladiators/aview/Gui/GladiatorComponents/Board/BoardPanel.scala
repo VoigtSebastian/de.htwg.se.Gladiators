b6f1a401204a461de63f21b677ea507578166669
VoigtSebastian <VoigtSebastian@protonmail.com>
2020-10-12 16:48:07 +0200

    Add tiles to Gui

--- src/main/scala/de/htwg/se/gladiators/aview/Gui/GladiatorComponents/Board/BoardPanel.scala
+++ src/main/scala/de/htwg/se/gladiators/aview/Gui/GladiatorComponents/Board/BoardPanel.scala
@@ -6,16 +6,16 @@ import scala.swing._
 import de.htwg.se.gladiators.aview.Gui.GladiatorComponents.Board.BoardLabel
 import de.htwg.se.gladiators.model.TileType
 
-case class BoardPanel(length: Int) extends GridPanel(length, length) with Publisher {
-    val tiles = (0 to (length - 1))
-        .map(y => (0 to (length - 1)).map(x => new BoardLabel(x, y, TileType.Sand)(tileClicked)).toVector)
+case class BoardPanel(length: Int, tiles: Vector[Vector[TileType]]) extends GridPanel(length, length) with Publisher {
+    val tileLabels = (0 to (length - 1))
+        .map(y => (0 to (length - 1)).map(x => new BoardLabel(x, y, tiles(y)(x))(tileClicked)).toVector)
         .toVector
 
-    tiles.foreach(_.foreach(contents += _))
+    tileLabels.foreach(_.foreach(contents += _))
 
     def tileClicked(tile: Coordinate) = publish(TileClicked(tile))
 
-    def selectTile(coordinate: Coordinate) = tiles(coordinate.y)(coordinate.x).select
-    def deselectTile(coordinate: Coordinate) = tiles(coordinate.y)(coordinate.x).deselect
+    def selectTile(coordinate: Coordinate) = tileLabels(coordinate.y)(coordinate.x).select
+    def deselectTile(coordinate: Coordinate) = tileLabels(coordinate.y)(coordinate.x).deselect
 
 }
