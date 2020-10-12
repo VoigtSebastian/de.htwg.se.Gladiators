9c93593a0babd98926b1f4fdfbd3b34d9ca38672
VoigtSebastian@protonmail.com <sebastian@DESKTOP-4R00RD2.localdomain>
2020-10-12 14:39:07 +0200

    Update naming panel

--- src/main/scala/de/htwg/se/gladiators/aview/Gui/GladiatorComponents/ShowPlayerName.scala
+++ src/main/scala/de/htwg/se/gladiators/aview/Gui/GladiatorComponents/ShowPlayerName.scala
@@ -5,10 +5,10 @@ import scala.swing._
 case class ShowPlayerName(playerDescription: String)(buttonCall: String => Unit) extends BoxPanel(Orientation.Vertical) {
     val textArea = GTextField("")
     contents += Swing.VStrut(2)
-    contents += GLabel(f"Whats the name of $playerDescription")
-    contents += Swing.VStrut(2)
+    contents += new BorderPanel { add(GLabel(f"Whats the name of $playerDescription"), BorderPanel.Position.Center) }
+    contents += Swing.VStrut(4)
     contents += textArea
-    contents += Swing.VStrut(2)
-    contents += GButton("Submit")(buttonCall(textArea.text))
-    border = Swing.EmptyBorder(2, 2, 2, 2)
+    contents += Swing.VStrut(4)
+    contents += new BorderPanel { add(GButton("Submit")(buttonCall(textArea.text)), BorderPanel.Position.Center) }
+    border = Swing.EmptyBorder(8, 8, 8, 8)
 }
